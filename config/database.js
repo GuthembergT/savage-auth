903ab49e40d5d376e5ce5af91bc9d1a8e6db0b37
Guthemberg Teixeira <guthembergteixeira@gmail.com>
2021-03-27 19:21:52 -0400

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://admin:123@cluster0.9nt6d.mongodb.net/savageAuth?retryWrites=true&w=majority', 
+    'dbName': 'savageAuth'
+};
